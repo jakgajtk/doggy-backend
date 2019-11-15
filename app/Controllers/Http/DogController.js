9873552e3e9ae1d7d9b525fab326be0a86fd7695
Jakub Gajtkowski <jakub.gajtkowski@gmail.com>
2019-11-15 18:22:17 +0700

    Added POST for dogs.

--- app/Controllers/Http/DogController.js
+++ app/Controllers/Http/DogController.js
@@ -6,6 +6,13 @@ class DogController {
   async index () {
     return await Dog.all()
   }
+
+  async store ({ request }) {
+    const dogData = request.only(['name', 'description', 'photo'])
+    console.log(dogData);
+    await Dog.create(dogData)
+    return await Dog.all()
+  }
 }
 
 module.exports = DogController
--- start/routes.js
+++ start/routes.js
@@ -18,4 +18,5 @@ const Route = use('Route')
 
 Route.on('/').render('welcome')
 
-Route.get('dogs', 'DogController.index')
+Route.resource('dogs', 'DogController')
+  .apiOnly()
