0ddc85bfbcf6b684f4fdeb5e874f2e50faa45e04
Jakub Gajtkowski <jakub.gajtkowski@gmail.com>
2019-11-25 20:58:43 +0700

    Add destroy dog possibility.

--- app/Controllers/Http/DogController.js
+++ app/Controllers/Http/DogController.js
@@ -9,8 +9,14 @@ class DogController {
 
   async store ({ request }) {
     const dogData = request.only(['name', 'description', 'photo'])
-    console.log(dogData);
     await Dog.create(dogData)
+    return await Dog.all()
+  }
+
+  async destroy ({ request }) {
+    const id = request.get().id
+    const dog = await Dog.find(id)
+    await dog.delete()
     return await Dog.all()
   }
 }
